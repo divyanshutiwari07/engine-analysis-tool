6bdc133db9cc2ad1d3c46e64b34f0942fcc0160d
divyanshutiwari07 <divyanshutiwari07@gmail.com>
2020-03-20 19:47:19 +0530

    Created part list and added bootstrap, font awesome, angular material

--- src/app/part-scanner/common/material/material.module.ts
+++ src/app/part-scanner/common/material/material.module.ts
@@ -1,42 +1,42 @@
-// import { NgModule} from '@angular/core';
-// import {
-//   MatButtonModule,
-//   MatTableModule,
-//   MatInputModule,
-//   MatSortModule,
-//   MatPaginatorModule,
-//   MatDatepickerModule,
-//   MatNativeDateModule,
-//   MatTabsModule,
-//   MatSelectModule,
-//   MatProgressSpinnerModule,
-//   MatMenuModule,
-//   MatIconModule,
-//   MatGridListModule,
-//   MatCardModule,
-//   MatExpansionModule
-// } from '@angular/material';
+import { NgModule} from '@angular/core';
+import {
+  MatButtonModule,
+  MatTableModule,
+  MatInputModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTabsModule,
+  MatSelectModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+  MatIconModule,
+  MatGridListModule,
+  MatCardModule,
+  MatExpansionModule
+} from '@angular/material';
 
-// const Material = [
-//   MatButtonModule,
-//   MatTableModule,
-//   MatInputModule,
-//   MatSortModule,
-//   MatPaginatorModule,
-//   MatDatepickerModule,
-//   MatNativeDateModule,
-//   MatTabsModule,
-//   MatSelectModule,
-//   MatProgressSpinnerModule,
-//   MatMenuModule,
-//   MatIconModule,
-//   MatGridListModule,
-//   MatCardModule,
-//   MatExpansionModule
-// ];
+const Material = [
+  MatButtonModule,
+  MatTableModule,
+  MatInputModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatTabsModule,
+  MatSelectModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+  MatIconModule,
+  MatGridListModule,
+  MatCardModule,
+  MatExpansionModule
+];
 
-// @NgModule({
-//   imports: [Material],
-//   exports: [Material]
-// })
-// export class MaterialModule { }
+@NgModule({
+  imports: [Material],
+  exports: [Material]
+})
+export class MaterialModule { }
--- src/app/part-scanner/part-scanner.module.ts
+++ src/app/part-scanner/part-scanner.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NgbActiveModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { PartScannerRoutingModule } from './part-scanner-routing.module';
 import { PartScannerComponent } from './part-scanner.component';
@@ -8,6 +9,7 @@ import { ScanPartComponent } from './components/scan-part/scan-part.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CameraComponent } from './components/camera/camera.component';
+import { MaterialModule } from './common/material/material.module';
 
 import { WebcamModule } from 'ngx-webcam';
 
@@ -22,12 +24,17 @@ import { WebcamModule } from 'ngx-webcam';
   ],
   imports: [
     CommonModule,
+    NgbModule,
     PartScannerRoutingModule,
-    WebcamModule
+    WebcamModule,
+    MaterialModule
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
     NO_ERRORS_SCHEMA
+  ],
+  providers: [
+    NgbActiveModal
   ]
 })
 export class PartScannerModule { }
